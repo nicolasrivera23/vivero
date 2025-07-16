9f0d17a9f9b9481137c2d18945b3dea2772157fa
nicolasrivera23 <113543987+nicolasrivera23@users.noreply.github.com>
2025-07-16 18:20:38 -0300

    first commit

---
+++ js/register.js
@@ -0,0 +1,28 @@
+let txtnombre = document.getElementById('nombre').value;
+let txtapellido = document.getElementById('apellido').value;
+let txtemail = document.getElementById('email').value;
+let txtpassword = document.getElementById('password');
+
+/* let btn = document.getElementById('btnRegistrar');
+btn.addEventListener('click', mostrarMensaje);
+function mostrarMensaje(evt) {
+    evt.preventDefault(); // Evita el envío del formulario
+    document.getElementById('mensaje').innerHTML = "Registro exitoso. Bienvenido ".concat(txtnombre, " ", txtapellido, "!");
+}  */
+
+let btn = document.getElementById('btnRegistrar');
+btn.addEventListener('click', function(event) {
+  event.preventDefault(); // Evita el envío del formulario
+    if (txtnombre === "" || txtapellido === "" || txtemail === "" || txtpassword.value.length < 1) {
+        alert("Por favor, complete todos los campos.");
+    } else {
+        alert("Registro exitoso. Bienvenido " + txtnombre + " " + txtapellido + "!");
+  // Limpiar los campos del formulario
+        document.getElementById("nombre").value = "";
+        document.getElementById("apellido").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("password").value = "";
+        document.getElementById("mensaje").innerHTML = "";
+
+    }
+}); 
